f9ecd5b28ff40033092b02c3bf9f7771ef7f3221
Sondus-Qasem <81189893+Sondus-Qasem@users.noreply.github.com>
2023-11-26 21:35:46 +0200

    git add .  git commit -m "WriteMethod2"  git push origin main

--- mid.py
+++ mid.py
@@ -6,3 +6,9 @@ def open_tab(): #method1
     url = input("Enter the URL: ")
     new_tab = {"Title": title, "URL": url, "Nested Tabs": []}
     open_tabs.append(new_tab)
+ # Function to close a tab
+def close_tab():
+    index = int(input("Enter the index of the tab to close (or press Enter to close the last opened tab): "))
+    if index:
+        closed_tab = open_tabs.pop(index)
+        print(f"Closed Tab: {closed_tab['Title']}"
