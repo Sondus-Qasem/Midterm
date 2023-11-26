f6b27876eb25d24e84673bda75272a5abc129052
Sondus-Qasem <81189893+Sondus-Qasem@users.noreply.github.com>
2023-11-26 21:31:31 +0200

    git add .  git commit -m "WriteMethod1"  git push origin main

--- mid.py
+++ mid.py
@@ -2,3 +2,7 @@ open_tabs=[] #global variables
 nested_tabs={} #global variables
 # Function to open a new tab
 def open_tab(): #method1
+  title = input("Enter the title of the website: ") # the user to input
+    url = input("Enter the URL: ")
+    new_tab = {"Title": title, "URL": url, "Nested Tabs": []}
+    open_tabs.append(new_tab)
